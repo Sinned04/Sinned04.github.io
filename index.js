4a9811ad70a73ba1b9e10d5c24eec027a1c81fba
Sinned04 <delacuadradennis40@gmail.com>
2024-05-25 12:16:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-1. const btnlike1=document.getElementById("btnLike1")
+ const btnlike1=document.getElementById("btnLike1")
+ const btnLike2=document.getElementById("btnLike2")
+ const countLikes1=document.getElementById("countLikes1")
+ const counLikes2=document.getElementById("countLikes2")
 
-2. const btnLike2=document.getElementById("btnLike2")
-
-3. const countLikes1=document.getElementById("countLikes1")
-
-4. const counLikes2=document.getElementById("countLikes2")
-
-5.
-
-
-
-6. function hitLikes1() {
-
-7.        let noLikes1=parseInt(countLikes.value) + 1
-
-8. countLikes1.textContent=totalLikes1.toString()
-
-9. }
-
-10. btnLike1.addEventListener("click",hitLikes1)
+function hitLikes1() {
+     let totalLikes1=parseInt(countLikes.value) + 1
+     countLikes1.textContent=totalLikes1.toString()
+ }
+ btnLike1.addEventListener("click",hitLikes1)
