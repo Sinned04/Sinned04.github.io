760a7d5bce52347087b400e62a4a7e514be47d42
Sinned04 <delacuadradennis40@gmail.com>
2024-06-08 11:07:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
-
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
-const btnDislike1 = document.getElementById("btnDislike1")
-const btnDislike2 = document.getElementById("btnDislike2")
-
-const countDislikes1 = document.getElementById("countDislikes1")
-const countDislikes2 = document.getElementById("countDislikes2")
+const btndisLike1 = document.getElementById("btndisLike1")
+const btndisLike2 = document.getElementById("btndisLike2")
+const countdisLikes1 = document.getElementById("countdisLikes1")
+const countdisLikes2 = document.getElementById("countdisLikes2")
 
 function hitLikes1() {
-   let totalLikes1 = parseInt(countLikes1.value) + 1
-   countLikes1.textContent = totalLikes1.toString()
+  let totalLikes1 = parseInt(countLikes1.value) + 1
+  countLikes1.textContent = totalLikes1.toString()
 }
 btnLike1.addEventListener("click",hitLikes1)
 
 function hitLikes2() {
-   let totalLikes2 = parseInt(countLikes1.value) + 1
-   countLikes2.textContent = totalLikes2.toString() 
-} 
+  let totalLikes2 = parseInt(countLikes2.value) + 1
+  countLikes2.textContent = totalLikes2.toString()
+}
 btnLike2.addEventListener("click",hitLikes2)
 
-function Dislikes1() {
-  let totalDislikes1 = parseInt(countDislikes1.value) + 1
-   countDislikes1.textContent = totalDislikes1.toString()
+function hitdisLikes1() {
+  let totaldisLikes1 = parseInt(countdisLikes1.value) + 1
+  countdisLikes1.textContent = totaldisLikes1.toString()
 }
-btnDislike1.addEventListener("click",Dislikes1)
+btndisLike1.addEventListener("click",hitdisLikes1)
 
-function Dislikes2() {
-  let totalDislikes2 = parseInt(countDislikes2.value) + 1
-   countDislikes2.textContent = totalDislikes2.toString()
+function hitdisLikes2() {
+  let totaldisLikes2 = parseInt(countdisLikes2.value) + 1
+  countdisLikes2.textContent = totaldisLikes2.toString()
 }
-btnDislike2.addEventListener("click",Dislikes2)
+btndisLike2.addEventListener("click",hitdisLikes2)
